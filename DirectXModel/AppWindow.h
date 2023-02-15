c1714b008e92aa4988d0bdccd32e976a4019ad14
Aleksandr Limarev <limarev.sanya@gmail.com>
2023-02-15 03:51:54 +0100

    3D rotation cube
    Got a lot of code from previous practice

--- DirectXModel/AppWindow.h
+++ DirectXModel/AppWindow.h
@@ -4,11 +4,20 @@
 #include "SwapChain.h"
 #include "DeviceContext.h"
 #include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "ConstantBuffer.h"
 #include "VertexShader.h"
+#include "PixelShader.h"
+
+
 class AppWindow : public Window
 {
 public:
 	AppWindow();
+
+	void updateQuadPosition();
+
+
 	~AppWindow();
 
 	// Inherited via Window
@@ -19,4 +28,16 @@ private:
 	SwapChain* m_swap_chain;
 	VertexBuffer* m_vb;
 	VertexShader* m_vs;
+	PixelShader* m_ps;
+	ConstantBuffer* m_cb;
+	IndexBuffer* m_ib;
+
+private:
+	long m_old_delta;
+	long m_new_delta;
+	float m_delta_time;
+
+	float m_delta_pos;
+	float m_delta_scale;
+	float m_delta_rot;
 };
--- DirectXModel/DeviceContext.h
+++ DirectXModel/DeviceContext.h
@@ -1,25 +1,33 @@
 #pragma once
 #include <d3d11.h>
+#include "Prerequisites.h"
 
-class SwapChain;
-class VertexBuffer;
-class VertexShader;
 
 class DeviceContext
 {
 public:
-	DeviceContext(ID3D11DeviceContext* device_context);
+	DeviceContext(ID3D11DeviceContext* device_context, RenderSystem* system);
 	void clearRenderTargetColor(SwapChain* swap_chain, float red, float green, float blue, float alpha);
 	void setVertexBuffer(VertexBuffer* vertex_buffer);
+	void setIndexBuffer(IndexBuffer* index_buffer);
+
+
 	void drawTriangleList(UINT vertex_count, UINT start_vertex_index);
+	void drawIndexedTriangleList(UINT index_count, UINT start_vertex_index, UINT start_index_location);
 	void drawTriangleStrip(UINT vertex_count, UINT start_vertex_index);
 
 	void setViewportSize(UINT width, UINT height);
 
 	void setVertexShader(VertexShader* vertex_shader);
+	void setPixelShader(PixelShader* pixel_shader);
 
-	bool release();
+	void setConstantBuffer(VertexShader* vertex_shader, ConstantBuffer* buffer);
+	void setConstantBuffer(PixelShader* pixel_shader, ConstantBuffer* buffer);
+
 	~DeviceContext();
 private:
 	ID3D11DeviceContext* m_device_context;
+	RenderSystem* m_system = nullptr;
+private:
+	friend class ConstantBuffer;
 };
